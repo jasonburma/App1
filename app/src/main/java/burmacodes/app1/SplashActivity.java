095d5ff6840b13faddb643681ceec158007bf1b6
joelytk <joel.yap@drake.edu>
2017-10-21 16:26:27 -0500

    splash screen

---
+++ app/src/main/java/burmacodes/app1/SplashActivity.java
@@ -0,0 +1,45 @@
+package burmacodes.app1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private TextView appName;
+    private ImageView appLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appName = (TextView) findViewById(R.id.app_name);
+        appLogo = (ImageView) findViewById(R.id.app_logo);
+
+        Animation appAnim = AnimationUtils.loadAnimation(this,R.anim.app_transition);
+        appName.startAnimation(appAnim);
+        appLogo.startAnimation(appAnim);
+
+        final Intent i = new Intent(this, Acts_Now_Activity.class);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+                timer.start();
+    }
+}
